2ae4131a01a0d7afa46ce5691bb7a5d0b61139f1
Ricardo Ferreira Pariz Silva <ricardoferreirapariz@gmail.com>
2017-06-21 09:23:05 -0300

    Arrumando função de mandar email

--- js/main.js
+++ js/main.js
@@ -4,29 +4,29 @@ $(document).ready(function(){
   // Init Material scripts for buttons ripples, inputs animations etc, more info on the next link https://github.com/FezVrasta/bootstrap-material-design#materialjs
   $.material.init();
 
-  $("#form").on("submit", function(e){
-    e.preventDefault();
-    $("#sub").attr("disabled", true);
-    console.log("foi");
-    var nome = $("#nome").val();
-    var message = $("#message").val();
-    var email = $("#email").val();
-    $.ajax({
-      url: "http://formspree.io/ricardoferreirapariz@gmail.com",
-      method: "POST",
-      data: {message: message, name: nome, email: email, _subject: "Portifolio"},
-      dataType: "json",
-      complete: function(){
-        iziToast.success({
-          title: 'Ok',
-          message: "Mensagem enviada, aguarde a resposta",
-          icon: "fa fa-check"
-        });
-        $("#sub").attr("disabled", false);
-        $("#form").each(function() {
-          this.reset();
-        });
-      }
-    });
-  });
+  // $("#form").on("submit", function(e){
+  //   e.preventDefault();
+  //   $("#sub").attr("disabled", true);
+  //   console.log("foi");
+  //   var nome = $("#nome").val();
+  //   var message = $("#message").val();
+  //   var email = $("#email").val();
+  //   $.ajax({
+  //     url: "http://formspree.io/ricardoferreirapariz@gmail.com",
+  //     method: "POST",
+  //     data: {message: message, name: nome, email: email, _subject: "Portifolio"},
+  //     dataType: "json",
+  //     complete: function(){
+  //       iziToast.success({
+  //         title: 'Ok',
+  //         message: "Mensagem enviada, aguarde a resposta",
+  //         icon: "fa fa-check"
+  //       });
+  //       $("#sub").attr("disabled", false);
+  //       $("#form").each(function() {
+  //         this.reset();
+  //       });
+  //     }
+  //   });
+  // });
 });
