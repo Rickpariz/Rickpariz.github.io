9ad877e1e43889e9e39feab370e56c081af5d10f
Ricardo Ferreira Pariz Silva <ricardoferreiraparizsilva85@gmail.com>
2018-08-24 22:27:52 -0300

    Adicionando formulário de contato

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,38 +5,38 @@ $(document).ready(function() {
     var name = $(this).find("input[name='name']").val();
     var email = $(this).find("input[name='email']").val();
     var message = $(this).find("textarea[name='message']").val();
-    $.ajax({
-      url: "http://formspree.io/ricardoferreirapariz@gmail.com",
-      method: "POST",
-      data: {message: message, name: name, email: email, _subject: "Portifolio"},
-      dataType: "json",
-      complete: function(response){
-        console.log(response);
-        iziToast.success({
-          title: 'Ok',
-          message: "Mensagem enviada, aguarde a resposta",
-          icon: "fa fa-check"
-        });
-        $("#sub").attr("disabled", false);
-        $("#form").each(function() {
-          this.reset();
-        });
-      }
-    });
-
-    // $.post("http://formspree.io/ricardoferreirapariz@gmail.com", {
-    //   message: message,
-    //   name: name,
-    //   email: email,
-    //   _subject: "Portifolio"
-    // }).done(function(response) {
-    //   console.log(response);
-    //   iziToast.success({title: 'Ok', message: "Mensagem enviada, aguarde a resposta", icon: "fa fa-check"});
-    //   $("#sub").attr("disabled", false);
-    //   $("#form").each(function() {
-    //     this.reset();
-    //   });
+    // $.ajax({
+    //   url: "http://formspree.io/ricardoferreirapariz@gmail.com",
+    //   method: "POST",
+    //   data: {message: message, name: name, email: email, _subject: "Portifolio"},
+    //   dataType: "json",
+    //   complete: function(response){
+    //     console.log(response);
+    //     iziToast.success({
+    //       title: 'Ok',
+    //       message: "Mensagem enviada, aguarde a resposta",
+    //       icon: "fa fa-check"
+    //     });
+    //     $("#sub").attr("disabled", false);
+    //     $("#form").each(function() {
+    //       this.reset();
+    //     });
+    //   }
     // });
+
+    $.post("https://formspree.io/ricardoferreirapariz@gmail.com", {
+      message: message,
+      name: name,
+      email: email,
+      _subject: "Portifolio"
+    }).done(function(response) {
+      console.log(response);
+      iziToast.success({title: 'Ok', message: "Mensagem enviada, aguarde a resposta", icon: "fa fa-check"});
+      $("#sub").attr("disabled", false);
+      $("#form").each(function() {
+        this.reset();
+      });
+    });
 
   });
 });
